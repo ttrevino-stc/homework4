b2bcc6eb794537367443a5e08e06bd4708af4098
Thomas Trevino <ttrevino@stu.southtexascollege.edu>
2025-02-15 23:32:50 -0600

    completed expense tracker

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
   import AddTransaction from './components/AddTransaction.vue';
   import TransactionList from './components/TransactionList.vue';
 
-  import {computed, ref} from 'vue';
+  import {ref, computed, onMounted} from 'vue'
 
   const transactions = ref([])
 
@@ -33,14 +33,34 @@
 
   const handleTransaction = (transactionData) => {
     transactions.value.push({
+      id: generateID(),
       text: transactionData.text,
       amount: transactionData.amount,
     })
+    saveToLocalStorage()
   }
 
-    const handleDelete = (id) => {
-      transactions.value = transactions.value.filter((x) => x.id !== id)
+  const generateID = () =>{
+    return Math.floor(Math.random()*10000000)
+  }
+
+  const handleDelete = (id) => {
+    transactions.value = transactions.value.filter((x) => x.id !== id)
+    saveToLocalStorage()
+  }
+
+  const saveToLocalStorage = () => {
+    localStorage.setItem('transactions', JSON.stringify(transactions.value))
+  }
+
+  onMounted( () => {
+    const savedTransactions = JSON.parse(localStorage.getItem('transactions'))
+
+    if(savedTransactions){
+      transactions.value = savedTransactions
     }
+  })
+    
 
 </script>
 
